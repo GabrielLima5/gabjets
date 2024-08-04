f0f217c1b4188c320377931aa5ee0f0a9c02ac6f
Gabriel Lima <gabriellpbiel@hotmail.com>
2024-08-03 22:28:35 -0300

    obtendo ano dinamico - footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,10 @@
-import styles from './Footer.module.css'
+import styles from "./Footer.module.css";
 
-export default function Footer(){
-    return(
-        <footer className={styles.footer}>
-            <span>&copy; 2023 - GabJets</span>
-            <span>Todos os direitos reservados.</span>
-        </footer>
-    )
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <span>&copy; {new Date().getFullYear()} - GabJets</span>
+      <span>Todos os direitos reservados.</span>
+    </footer>
+  );
 }
